891132e3d67a35fa101c43077b4482e87bd46466
Nahukre <79340552+Nahukre@users.noreply.github.com>
2021-08-07 17:35:08 -0300

    cambios

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -13,12 +13,6 @@ class persona {
     }
 }
 
-const multiplicacion = (a, b) => a * b;
-const porciento = x => x * 0.01;
-const division = (a, b) => a / b;
-const suma = (a, b) => a + b;
-
-
 let nombre = prompt("Cuál es su nombre?");
 let mail = prompt("Ingrese su mail");
 let sueldo = parseInt(prompt("Indique cuál es su sueldo"));
@@ -27,18 +21,57 @@ let extra = parseInt(prompt("Si tiene algún ingreso extra destinado a ahorros e
 let objetivo = prompt("Qué bien desea alcanzar con sus ahorros?");
 let valorBien = parseInt(prompt("Cuál es el valor del bien que desea alcanzar con sus ahorros?"));
 
+const nuevaPersona = () => {
+    const persona1 = new persona(nombre, mail, sueldo, ahorro, extra, objetivo, valorBien);
+    console.log(persona1.toString());
+    alert("Bienvenido " + persona1.nombre);
+}
+nuevaPersona();
 
-let ahorroPorcentaje = porciento(multiplicacion(sueldo, ahorro));
-let ahorroAnual = suma((multiplicacion(ahorro, 12)), extra);
-let mesesDeAhorro = division(valorBien, sueldo);
-let ahorroReal = division(valorBien, ahorro);
-let sueldoAños = division(mesesDeAhorro, 12);
-let ahorroAños = division(ahorroReal, 12);
-let tiempoTotal = division(valorBien, ahorroAnual);
-let tiempoPlus = division(valorBien, multiplicacion(ahorroAnual, 1.1));
+const resultadoFinanciero = () => {
+    const multiplicacion = (a, b) => a * b;
+    const porciento = x => x * 0.01;
+    const division = (a, b) => a / b;
+    const suma = (a, b) => a + b;
 
-const persona1 = new persona(nombre, mail, sueldo, ahorro, extra, objetivo, valorBien);
-console.log(persona1.toString());
+    let ahorroPorcentaje = division((multiplicacion(ahorro, 100)), sueldo);
+    let ahorroAnual = suma((multiplicacion(ahorro, 12)), extra);
+    let mesesDeAhorro = division(valorBien, sueldo);
+    let ahorroReal = division(valorBien, ahorro);
+    let sueldoAños = division(mesesDeAhorro, 12);
+    let ahorroAños = division(ahorroReal, 12);
+    let tiempoTotal = division(valorBien, ahorroAnual);
+    let tiempoPlus = division(valorBien, multiplicacion(ahorroAnual, 1.1));
+
+    switch (true) {
+        case (ahorroPorcentaje <= 5):
+            console.log("Su porcentaje de ahorro es bajo.");
+            break;
+        case ((ahorroPorcentaje > 5) && (ahorroPorcentaje <= 10)):
+            console.log("Su porcentaje de ahorro podría ser más elevado.");
+            break;
+        case ((ahorroPorcentaje > 10) && (ahorroPorcentaje <= 20)):
+            console.log("Su porcentaje de ahorro es considerable.");
+            break;
+        case ((ahorroPorcentaje > 20) && (ahorroPorcentaje <= 50)):
+            console.log("Su porcentaje de ahorro es elevado.");
+            break;
+        case (ahorroPorcentaje > 50):
+            console.log("Usted tiene una gran capacidad de ahorro.");
+            break;
+        default:
+            console.log("Usted no ahorra o ingresó un dato incorrecto.");
+            break;
+    }
+
+    console.log("Su porcentaje de ahorro mensual es del " + ahorroPorcentaje + "%.");
+    console.log("Su ahorro anual es de $" + ahorroAnual + ".");
+    console.log("Si destinara todo su sueldo a comprar su " + objetivo + " tardaría " + mesesDeAhorro + " meses o " + sueldoAños + " años para comprar lo que desea.");
+    console.log("Si destinara solo su ahorro mensual a comprar su " + objetivo + " tardaría " + ahorroReal + " meses o " + ahorroAños + " años para comprar lo que desea.");
+    console.log("Si destinara su ahorro mensual más sus ingresos extra anuales a comprar su " + objetivo + " tardaría " + tiempoTotal + " años para comprar lo que desea.");
+    console.log("Si consiguiera aumentar su ahorro anual un 10 % para adquirir su " + objetivo + " tardaría " + tiempoPlus + " años para comprar lo que desea.");
+}
+resultadoFinanciero();
 
 //array
 class Inversion {
@@ -58,12 +91,13 @@ class activoDeInversion {
         this.activo.push(inversion);
     }
 }
-const nacional = new activoDeInversion("Activos dentro del mercado local");
-const dolarBlue = new Inversion("Dolar blue", "peso", 170, "moneda norteamericana en el mercado informal");
-const bitcoin = new Inversion("Bitcoin", "dolar", 35000, "Criptomoneda");
-const cedearTesla = new Inversion("Testa cedear", "peso", 7500, "Cedears de la compañia Tesla. Activo financiero atado al dolar y a la valuación de la empresa en el mercado.");
-const cedearApple = new Inversion("Apple cedear", "peso", 3500, "Cedears de la compañia Apple. Activo financiero atado al dolar y a la valuación de la empresa en el mercado.");
-const cedearAmazon = new Inversion("Amazon cedear", "peso", 4500, "Cedears de la compañia Amazon. Activo financiero atado al dolar y a la valuación de la empresa en el mercado.");
+
+let nacional = new activoDeInversion("Activos dentro del mercado local con los cuales poder ahorrar");
+let dolarBlue = new Inversion("Dolar blue", "peso", 170, "Moneda norteamericana en el mercado informal");
+let bitcoin = new Inversion("Bitcoin", "dolar", 35000, "Criptomoneda");
+let cedearTesla = new Inversion("Testa cedear", "peso", 7500, "Cedears de la compañia Tesla. Activo financiero atado al dolar y a la valuación de la empresa en el mercado.");
+let cedearApple = new Inversion("Apple cedear", "peso", 3500, "Cedears de la compañia Apple. Activo financiero atado al dolar y a la valuación de la empresa en el mercado.");
+let cedearAmazon = new Inversion("Amazon cedear", "peso", 4500, "Cedears de la compañia Amazon. Activo financiero atado al dolar y a la valuación de la empresa en el mercado.");
 
 nacional.agregarInversion(dolarBlue);
 nacional.agregarInversion(bitcoin);
@@ -71,34 +105,5 @@ nacional.agregarInversion(cedearTesla);
 nacional.agregarInversion(cedearApple);
 nacional.agregarInversion(cedearAmazon);
 
+
 console.log(nacional.activo);
-
-
-switch (true) {
-    case (ahorroPorcentaje <= 5):
-        console.log("Su porcentaje de ahorro es bajo.");
-        break;
-    case ((ahorroPorcentaje > 5) && (ahorroPorcentaje <= 10)):
-        console.log("Su porcentaje de ahorro podría ser más elevado.");
-        break;
-    case ((ahorroPorcentaje > 10) && (ahorroPorcentaje <= 20)):
-        console.log("Su porcentaje de ahorro es considerable.");
-        break;
-    case ((ahorroPorcentaje > 20) && (ahorroPorcentaje <= 50)):
-        console.log("Su porcentaje de ahorro es elevado.");
-        break;
-    case (ahorroPorcentaje > 50):
-        console.log("Usted tiene una gran capacidad de ahorro.");
-        break;
-    default:
-        console.log("Usted no ahorra o ingresó un dato incorrecto.");
-        break;
-}
-
-
-console.log("Su porcentaje de ahorro mensual es del " + ahorroPorcentaje + "%.");
-console.log("Su ahorro anual es de $" + ahorroAnual + ".");
-console.log("Si destinara todo su sueldo a comprar su " + objetivo + " tardaría " + mesesDeAhorro + " meses o " + sueldoAños + " años para comprar lo que desea.");
-console.log("Si destinara solo su ahorro mensual a comprar su " + objetivo + " tardaría " + ahorroReal + " meses o " + ahorroAños + " años para comprar lo que desea.");
-console.log("Si destinara su ahorro mensual más sus ingresos extra anuales a comprar su " + objetivo + " tardaría " + tiempoTotal + " años para comprar lo que desea.");
-console.log("Si consiguiera aumentar su ahorro anual un 10 % para adquirir su " + objetivo + " tardaría " + tiempoPlus + " años para comprar lo que desea.");
